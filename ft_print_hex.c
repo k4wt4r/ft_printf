fbf1ce2808808f9c1490a936838acedff91d7ff4
k4wt4r <kawtarelalmi0@gmail.com>
2021-11-22 10:37:07 +0100

    fix

--- ft_print_hex.c
+++ ft_print_hex.c
@@ -1,65 +1,46 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_print_hex.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kel-alam <kel-alam@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/19 09:17:45 by kel-alam          #+#    #+#             */
-/*   Updated: 2021/11/19 17:57:31 by kel-alam         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "ft_printf.h"
+#include "libft.h"
 
-int	ft_length_hex(unsigned int number)
+int ft_hex_len(unsigned int num)
 {
-	int	len;
+	int len;
 
 	len = 0;
-	while (number != 0)
+	while (num != 0)
 	{
 		len++;
-		number /= 16;
+		num = num / 16;
 	}
 	return (len);
 }
 
-void	ft_put_hex(unsigned int number, const char *format)
+void ft_put_hex(unsigned int num, const char format)
 {
-	int len_number;
-
-	len_number = 0;
-	if (number >= 16)
+	if (num >= 16)
 	{
-		ft_put_hex(number / 16, format);
-		ft_put_hex(number % 16, format);
+		ft_put_hex(num / 16, format);
+		ft_put_hex(num % 16, format);
 	}
 	else
 	{
-		if (number < 10)
-		{
-			ft_putchar(number + '0');
-		}
+		if (num <= 9)
+			ft_putchar_fd((num + '0'), 1);
 		else
 		{
 			if (format == 'x')
-			{
-				ft_putchar((number - 10 + 'a') + '0');
-			}
+				ft_putchar_fd((num - 10 + 'a'), 1);
 			if (format == 'X')
-			{
-				ft_putchar((number - 10 + 'A') + '0');
-			}
+				ft_putchar_fd((num - 10 + 'A'), 1);
 		}
 	}
 }
 
-int	ft_print_hex(unsigned long int number, const char *format)
+int ft_print_hex(unsigned int num, const char format)
 {
-	if (number == 0)
+	if (num == 0)
 		return (write(1, "0", 1));
 	else
-		ft_print_hex(number, format);
-	return (ft_length_hex(number));
+		ft_put_hex(num, format);
+	return (ft_hex_len(num));
 }
